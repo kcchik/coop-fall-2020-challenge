98390666bed915a2531cb3af5256657eea610eba
Kevin Chik <kcchik@uwaterloo.ca>
2020-07-10 14:38:27 -0400

    Complete

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,50 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.index = 0;
+    this.history = [];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.value += num;
+    this.index++;
+    this.history.splice(this.index, 0, num);
+  }
+
+  subtract(num) {
+    this.value -= num;
+    this.index++;
+    this.history.splice(this.index, 0, -num);
+  }
+
+  undo() {
+    this.bulk_undo(1);
+  }
+
+  redo() {
+    this.bulk_redo(1);
+  }
+
+  bulk_undo(num) {
+    if (this.index >= num) {
+      this.index -= num;
+      const actions = this.history.slice(this.index, this.index + num);
+      for (const action of actions) {
+        this.value -= action;
+      }
+    }
+  }
+
+  bulk_redo(num) {
+    if (this.index > this.history.length - num) {
+      num = this.history.length - this.index;
+    }
+    const actions = this.history.slice(this.index - 1, this.index - 1 + num);
+    this.index += num;
+    for (const action of actions) {
+      this.value += action;
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
